dd9dc4212ac3f1b2583fcf96a1380dee885f77a2
railsgirls <railsgirls@lab2023.com>
2019-03-16 18:22:26 +0300

    Add pg gem for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/info'
   resources :ideas
   root to: redirect('/ideas')
+  get "page/info"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
